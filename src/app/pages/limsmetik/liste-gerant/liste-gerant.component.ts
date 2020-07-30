dd902bb1be4f5208c18629ff0b8ac29e3f2fab59
limamou <llibasse2010@gmail.com>
2020-07-30 21:36:59 +0200

    t

--- src/app/pages/limsmetik/liste-gerant/liste-gerant.component.ts
+++ src/app/pages/limsmetik/liste-gerant/liste-gerant.component.ts
@@ -117,6 +117,7 @@ export class ListeGerantComponent implements OnInit {
     this.id = idGer;
 
     this.service.GetGerantById(this.id).subscribe(data => {
+      console.log(data)
       this.gerAsupprim = data;
      
     }, err => {
@@ -136,7 +137,7 @@ export class ListeGerantComponent implements OnInit {
   
     this.service.deleteGer(c).subscribe(res => {
       if (res['success'] == true) {
-        this.showToast(this.status2, this.title3, this.content3);
+        this.showToast(this.status, this.title3, this.content3);
         this.magasin = localStorage.getItem('idmagasin');
        
         this.service.getAllGerantByMagasin(localStorage.getItem('idmagasin')).subscribe((data) => {this.gerant = data; }, (err) => {console.log(err); });
